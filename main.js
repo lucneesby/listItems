1ff6645d6248ce7bf7dfa6771e8003066a0910ee
Luc Neesby <Luc Neesby>
2017-05-31 18:03:21 -0700

    Added Item Counter

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var newItemCounter = 1;
+var ourList = document.getElementById("our-list");
+var ourButton = document.getElementById("our-button");
 var ourHeadline = document.getElementById("our-headline");
 var listItems = document.getElementById("our-list").getElementsByTagName("li");
 for (i=0; i<listItems.length; i++) {
@@ -6,4 +9,11 @@ for (i=0; i<listItems.length; i++) {
 
 function activateItem () {
   ourHeadline.innerHTML = this.innerHTML;
+}
+
+ourButton.addEventListener("click", createNewItem);
+
+function createNewItem() {
+    ourList.innerHTML += "<li>Something New " + newItemCounter + "</li>";
+    newItemCounter++;
 }
