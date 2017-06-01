d42766cab5498bb958461a0ca530ed1e08079a7f
Luc Neesby <Luc Neesby>
2017-05-31 21:27:05 -0700

    Added event listener for new clicks

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ var ourList = document.getElementById("our-list");
 var ourButton = document.getElementById("our-button");
 var ourHeadline = document.getElementById("our-headline");
 var listItems = document.getElementById("our-list").getElementsByTagName("li");
-for (i=0; i<listItems.length; i++) {
-  listItems[i].addEventListener("click", activateItem);
-}
-function activateItem () {
-  ourHeadline.innerHTML = this.innerHTML;
-  for (i=0; i<listItems.length; i++) {
-    listItems[i].classList.remove("active");
+ourList.addEventListener("click", activateItem);
+function activateItem (e) {
+  if(e.target.nodeName == "LI") {
+    ourHeadline.innerHTML = e.target.innerHTML;
+    for (i=0; i<e.target.parentNode.children.length; i++) {
+      listItems[i].classList.remove("active");
+    }
+    e.target.classList.add("active");
   }
-  this.classList.add("active");
 }
 
 ourButton.addEventListener("click", createNewItem);
